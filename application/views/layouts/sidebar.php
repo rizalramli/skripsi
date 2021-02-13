890da934bbf552b724b9604b439c3f5508f56e72
Mohamad Rizal Ramli <rizalramli993@gmail.com>
2021-02-13 11:33:27 +0700

    active menu

--- application/views/layouts/sidebar.php
+++ application/views/layouts/sidebar.php
@@ -8,13 +8,23 @@
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Transaction</li>
-            <li><a class="nav-link" href="<?php echo base_url('priority'); ?>"><i class="far fa-square"></i> <span>Prioritas Order</span></a></li>
-            <li><a class="nav-link" href="<?php echo base_url('calculate'); ?>"><i class="far fa-square"></i> <span>Perhitungan</span></a></li>
+            <li <?php if ($this->uri->segment(1) == "priority") {
+                    echo 'class="active"';
+                } ?>><a class="nav-link" href="<?php echo base_url('priority'); ?>"> <span class="ml-3">Prioritas Order</span></a></li>
+            <li <?php if ($this->uri->segment(1) == "calculate") {
+                    echo 'class="active"';
+                } ?>><a class="nav-link" href="<?php echo base_url('calculate'); ?>"> <span class="ml-3">Perhitungan</span></a></li>
 
             <li class="menu-header">Master</li>
-            <li><a class="nav-link" href="<?php echo base_url('criteria'); ?>"><i class="far fa-square"></i> <span>Kriteria</span></a></li>
-            <li><a class="nav-link" href="<?php echo base_url('product'); ?>"><i class="far fa-square"></i> <span>Barang</span></a></li>
-            <li><a class="nav-link" href="<?php echo base_url('user'); ?>"><i class="far fa-square"></i> <span>User</span></a></li>
+            <li <?php if ($this->uri->segment(1) == "criteria") {
+                    echo 'class="active"';
+                } ?>><a class="nav-link" href="<?php echo base_url('criteria'); ?>"> <span class="ml-3">Kriteria</span></a></li>
+            <li <?php if ($this->uri->segment(1) == "product") {
+                    echo 'class="active"';
+                } ?>><a class="nav-link" href="<?php echo base_url('product'); ?>"> <span class="ml-3">Barang</span></a></li>
+            <li <?php if ($this->uri->segment(1) == "user") {
+                    echo 'class="active"';
+                } ?>><a class="nav-link" href="<?php echo base_url('user'); ?>"> <span class="ml-3">User</span></a></li>
         </ul>
     </aside>
 </div>
